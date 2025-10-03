5b89e27c11556bd2ee2c36f13f32f19a6fe9b43a
McHaillet <martenchaillet@gmail.com>
2025-10-03 15:42:01 +0200

    add test to check for correct batches

--- tests/test_subpixel_crop_2d.py
+++ tests/test_subpixel_crop_2d.py
@@ -1,5 +1,6 @@
 import torch
 import einops
+import pytest
 
 from torch_subpixel_crop import subpixel_crop_2d
 
@@ -42,13 +43,23 @@ def test_subpixel_crop_multi_2d():
 
 def test_subpixel_crop_multi_batch_2d():
     batch = 3
-    image = torch.zeros((batch, 10, 10))
-    image[..., 4:6, 4:6] = 1
-
     positions = einops.repeat(
         torch.tensor([[4, 4], [5, 5]]).float(),
         'n yx -> n b yx', b=batch
     )
+
+    image = torch.zeros((2, 10, 10))
+    with pytest.raises(ValueError):
+        # mismatch in image and position batch should raise error
+        cropped_image = subpixel_crop_2d(
+            image=image,
+            positions=positions,
+            sidelength=4
+        )
+
+    image = torch.zeros((batch, 10, 10))
+    image[..., 4:6, 4:6] = 1
+
     cropped_image = subpixel_crop_2d(
         image=image,
         positions=positions,
